3f96072ec9df89b62b639bf98d2bbdc650f36c6a
PerfilevIS <163278781+PerfilevIS@users.noreply.github.com>
2024-03-18 06:38:59 +0700

    Add files via upload

---
+++ CarFacttory/CarFacttory/FactoryClass.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFacttory
+{
+    internal class FactoryClass
+  
+    {
+        public interface ICarFactory
+        {
+            Car CreateCar(string name, int maxSpeed, int distance);
+        }
+
+        public class SedanFactory : ICarFactory
+        {
+            public Car CreateCar(string name, int maxSpeed, int distance)
+            {
+                return new Sedan(name, maxSpeed, distance);
+            }
+        }
+
+        public class SUVFactory : ICarFactory
+        {
+            public Car CreateCar(string name, int maxSpeed, int distance)
+            {
+                return new SUV(name, maxSpeed, distance);
+            }
+        }
+
+        public abstract class Car
+        {
+            public string Name { get; set; }
+            public int MaxSpeed { get; set; }
+            public int Distance { get; set; }
+            public int CurrentSpeed { get; set; }
+            public int CurrentDistance { get; set; }
+
+            public Car(string name, int maxSpeed, int distance)
+            {
+                Name = name;
+                MaxSpeed = maxSpeed;
+                Distance = distance;
+                CurrentSpeed = 0;
+                CurrentDistance = 0;
+            }
+
+            public virtual void Move()
+            {
+                if (CurrentSpeed < MaxSpeed)
+                {
+                    CurrentSpeed++;
+                }
+                else
+                {
+                    CurrentSpeed--;
+                }
+                CurrentDistance += CurrentSpeed;
+            }
+
+            public bool IsFinished()
+            {
+                return CurrentDistance >= Distance;
+            }
+        }
+
+        public class Sedan : Car
+        {
+            public Sedan(string name, int maxSpeed, int distance) : base(name, maxSpeed, distance)
+            {
+            }
+        }
+
+        public class SUV : Car
+        {
+            public SUV(string name, int maxSpeed, int distance) : base(name, maxSpeed, distance)
+            {
+            }
+        }
+
+        public class Race
+        {
+            public List<Car> Cars { get; set; }
+            public int Distance { get; set; }
+
+            public Race(List<Car> cars, int distance)
+            {
+                Cars = cars;
+                Distance = distance;
+            }
+
+            public void Start()
+            {
+                while (!AllFinished())
+                {
+                    foreach (Car car in Cars)
+                    {
+                        car.Move();
+                        Console.WriteLine($"{car.Name}: {car.CurrentSpeed} km/h, {car.CurrentDistance} km");
+                    }
+                }
+                PrintResults();
+            }
+
+            private bool AllFinished()
+            {
+                foreach (Car car in Cars)
+                {
+                    if (!car.IsFinished())
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            private void PrintResults()
+            {
+                Cars.Sort((x, y) => y.CurrentDistance.CompareTo(x.CurrentDistance));
+                for (int i = 0; i < Cars.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {Cars[i].Name}");
+                }
+            }
+        }
+
+    }
+}
---
+++ CarFacttory/CarFacttory/Program.cs
@@ -0,0 +1,19 @@
+﻿using static CarFacttory.FactoryClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICarFactory sedanFactory = new SedanFactory();
+        ICarFactory suvFactory = new SUVFactory();
+
+        List<Car> cars = new List<Car>();
+        cars.Add(sedanFactory.CreateCar("Sedan 1", 150, 1000));
+        cars.Add(sedanFactory.CreateCar("Sedan 2", 160, 1000));
+        cars.Add(suvFactory.CreateCar("SUV 1", 180, 1000));
+        cars.Add(suvFactory.CreateCar("SUV 2", 190, 1000));
+
+        Race race = new Race(cars, 1000);
+        race.Start();
+    }
+}
